0536fe7ec73dc079bce39a0f9bb8e91026bf59e1
Sidgon <david.gsponer95@hotmail.com>
2018-01-26 16:17:07 +0200

    First commit

---
+++ src/main/java/com/david/gsponer/bookstore/domain/Book.java
@@ -0,0 +1,64 @@
+package com.david.gsponer.bookstore.domain;
+
+public class Book {
+
+	private String title, author, year, isbn;
+	private int price;
+	
+	public Book(String title, String author, String year, String isbn, int price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+}
